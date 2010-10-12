94d7dfbd0f195908032e062dc1231ca0a6642126
byteit101 <byteit101@0141d0df-edca-4edd-a601-3402401f7f08>
2010-10-12 20:35:30 +0000

    Added Taco (Contains negative value issue)
    git-svn-id: http://firstforge.wpi.edu/svn/repos/zomb/trunk@91 0141d0df-edca-4edd-a601-3402401f7f08

---
+++ ZomBdll/WPF/Controls/TacoMeter.cs
@@ -0,0 +1,71 @@
+﻿/*
+ * ZomB Dashboard System <http://firstforge.wpi.edu/sf/projects/zombdashboard>
+ * Copyright (C) 2009-2010, Patrick Plenefisch and FIRST Robotics Team 451 "The Cat Attack"
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace System451.Communication.Dashboard.WPF.Controls
+{
+    /// <summary>
+    /// Interaction logic for TacoMeter.xaml
+    /// </summary>
+    [Design.ZomBDesignable()]
+    public class TacoMeter : ZomBGLControl, IValueConverter, Design.IZomBDesignableControl
+    {
+        static TacoMeter()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(TacoMeter),
+            new FrameworkPropertyMetadata(typeof(TacoMeter)));
+        }
+
+        public TacoMeter()
+        {
+            this.Width = 150;
+            this.Height = 100;
+        }
+
+
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return ((double)value)*90-90;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        #region IZomBDesignableControl Members
+
+        public Design.ZomBDesignableControlInfo GetDesignInfo()
+        {
+            return new Design.ZomBDesignableControlInfo { Name = "Taco Meter", Description = "This shows -1 to 1, useful for eating", Type = typeof(TacoMeter) };
+        }
+
+        #endregion
+    }
+}
