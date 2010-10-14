c2d08b76b93c5b9fff0e1e77187f6d33ac43d1b2
byteit101 <byteit101@0141d0df-edca-4edd-a601-3402401f7f08>
2010-10-14 20:23:29 +0000

    Added DropShadowChrome to remove bitmap effect drop shadow
    git-svn-id: http://firstforge.wpi.edu/svn/repos/zomb/trunk@93 0141d0df-edca-4edd-a601-3402401f7f08

---
+++ Apps/VisualZomB/DropShadowChrome.cs
@@ -0,0 +1,44 @@
+﻿/*
+ * ZomB Dashboard System <http://firstforge.wpi.edu/sf/projects/zombdashboard>
+ * Copyright (C) 2009-2010, Patrick Plenefisch and FIRST Robotics Team 451 "The Cat Attack"
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows;
+
+namespace System451.Communication.Dashboard.ViZ
+{
+    class DropShadowChrome : Decorator
+    {
+        private static SolidColorBrush shadow;
+
+        static DropShadowChrome()
+        {
+            shadow = new SolidColorBrush(Color.FromArgb(128, 0, 0, 0));
+        }
+
+        protected override void OnRender(DrawingContext drawingContext)
+        {
+            Rect rect = new Rect(-1, -1, this.ActualWidth + 2.0, this.ActualHeight + 2.0);
+            drawingContext.DrawRectangle(Brushes.Black, null, rect);
+            rect = new Rect(1.0, 1.0, this.ActualWidth + 2.0, this.ActualHeight + 2.0);
+            drawingContext.DrawRectangle(shadow, null, rect);
+        }
+    }
+}
