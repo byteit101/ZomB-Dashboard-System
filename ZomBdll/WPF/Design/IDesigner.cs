aefb3bca7d3d89b503e71503f9c1d3f7e490c106
byteit101 <byteit101@0141d0df-edca-4edd-a601-3402401f7f08>
2010-10-30 01:33:36 +0000

    Adding new Designer interface
    git-svn-id: http://firstforge.wpi.edu/svn/repos/zomb/trunk@140 0141d0df-edca-4edd-a601-3402401f7f08

---
+++ ZomBdll/WPF/Design/IDesigner.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Reflection;
+
+namespace System451.Communication.Dashboard.WPF.Design
+{
+    public interface IDesigner
+    {
+        FrameworkElement GetProperyField(object obj, PropertyInfo property);
+    }
+}
