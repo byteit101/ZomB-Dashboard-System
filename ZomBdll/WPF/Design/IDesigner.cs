5912db98b776b21022df7e66297065056320c0df
byteit101 <byteit101@0141d0df-edca-4edd-a601-3402401f7f08>
2010-10-30 16:12:15 +0000

    Added Designable Attribute
    git-svn-id: http://firstforge.wpi.edu/svn/repos/zomb/trunk@147 0141d0df-edca-4edd-a601-3402401f7f08

--- ZomBdll/WPF/Design/IDesigner.cs
+++ ZomBdll/WPF/Design/IDesigner.cs
@@ -15,4 +15,15 @@ namespace System451.Communication.Dashboard.WPF.Design
         bool IsExpanded();
         string GetValue();
     }
+
+    [global::System.AttributeUsage(AttributeTargets.Class, Inherited = true, AllowMultiple = false)]
+    sealed class DesignerAttribute : Attribute
+    {
+        public DesignerAttribute(Type DesignerType)
+        {
+            this.DesignerType = DesignerType;
+        }
+
+        public Type DesignerType { get; private set; }
+    }
 }
