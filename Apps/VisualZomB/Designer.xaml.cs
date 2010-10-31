4df7c9934e12d7d8a7a88278145f3f3627d949ec
byteit101 <byteit101@0141d0df-edca-4edd-a601-3402401f7f08>
2010-10-31 21:22:20 +0000

    Added new VisualZomB logo & fade out on start
    git-svn-id: http://firstforge.wpi.edu/svn/repos/zomb/trunk@149 0141d0df-edca-4edd-a601-3402401f7f08

--- Apps/VisualZomB/Designer.xaml.cs
+++ Apps/VisualZomB/Designer.xaml.cs
@@ -36,6 +36,7 @@ using System451.Communication.Dashboard.WPF;
 using System451.Communication.Dashboard.WPF.Controls;
 using System451.Communication.Dashboard.WPF.Design;
 using System.Collections.ObjectModel;
+using System.Windows.Media.Animation;
 
 namespace System451.Communication.Dashboard.ViZ
 {
@@ -73,6 +74,10 @@ namespace System451.Communication.Dashboard.ViZ
             propHolder = tbx.PropertyBox;
             this.Top = (System.Windows.Forms.Screen.PrimaryScreen.WorkingArea.Height <= 600) ? 0 : 10;
             this.Left = (System.Windows.Forms.Screen.PrimaryScreen.WorkingArea.Width / 2.0) - this.Width / 2.0;
+            DoubleAnimation VizLogoani = new DoubleAnimation(1, 0, new Duration(new TimeSpan(0,0,3)));
+            VizLogoani.BeginTime = new TimeSpan(0, 0, 2);
+            VizLogoani.Completed+=delegate{ LayoutCvs.Children.Remove(ViZLogo); };
+            ViZLogo.BeginAnimation(Image.OpacityProperty, VizLogoani);
         }
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
