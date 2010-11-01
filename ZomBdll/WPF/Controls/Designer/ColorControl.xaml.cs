5bc674307a06d9b537e9ebd96e184efab2575991
byteit101 <byteit101@0141d0df-edca-4edd-a601-3402401f7f08>
2010-11-01 23:17:09 +0000

    Added ColorControl for color designing
    git-svn-id: http://firstforge.wpi.edu/svn/repos/zomb/trunk@151 0141d0df-edca-4edd-a601-3402401f7f08

---
+++ ZomBdll/WPF/Controls/Designer/ColorControl.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Globalization;
+
+namespace System451.Communication.Dashboard.WPF.Controls.Designer
+{
+    /// <summary>
+    /// Interaction logic for ColorControl.xaml
+    /// </summary>
+    public partial class ColorControl : UserControl, IValueConverter
+    {
+        public ColorControl()
+        {
+            InitializeComponent();
+
+            Binding b = new Binding("Fill");
+            b.Source = colorPrev;
+            b.Mode = BindingMode.TwoWay;
+            b.Converter = this;
+            SetBinding(ColorProperty, b);
+        }
+
+        public Color Color
+        {
+            get { return (Color)GetValue(ColorProperty); }
+            set { SetValue(ColorProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Color.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ColorProperty =
+            DependencyProperty.Register("Color", typeof(Color), typeof(ColorControl), new UIPropertyMetadata(new Color()));
+
+        #region IValueConverter Members
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null)
+                return null;
+            return (value as SolidColorBrush).Color;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null)
+                return null;
+            return new SolidColorBrush((System.Windows.Media.Color)value);
+        }
+
+        #endregion
+    }
+}
---
+++ ZomBdll/WPF/Controls/Designer/ColorControlColorSeperator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+using System.Globalization;
+using System.Windows.Media;
+
+namespace System451.Communication.Dashboard.WPF.Controls.Designer
+{
+    public class ColorControlColorSeperator : IMultiValueConverter
+    {
+        public object Convert(object[] value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return new SolidColorBrush(Color.FromArgb(System.Convert.ToByte(value[3]), System.Convert.ToByte(value[0]), System.Convert.ToByte(value[1]), System.Convert.ToByte(value[2])));
+        }
+
+        public object[] ConvertBack(object value, Type[] targetType, object parameter, CultureInfo culture)
+        {
+            var cr = (value as SolidColorBrush).Color;
+            return new object[] { cr.R, cr.G, cr.B, cr.A };
+        }
+    }
+}
