c9fe66fe15868816e4ab93e0682314f7e0ed73bd
byteit101 <byteit101@0141d0df-edca-4edd-a601-3402401f7f08>
2011-06-11 21:06:43 +0000

    Adding virtual TCP test sender
    git-svn-id: http://firstforge.wpi.edu/svn/repos/zomb/trunk@414 0141d0df-edca-4edd-a601-3402401f7f08

--- ZomBdll/NetAndDataProcessing/DashboardData/DashboardPacketEmulator.cs
+++ ZomBdll/NetAndDataProcessing/DashboardData/DashboardPacketEmulator.cs
@@ -110,4 +110,105 @@ namespace System451.Communication.Dashboard.Net
             catch { }
         }
     }
+
+    /// <summary>
+    /// Emulates the cRIO TCP in sending pre-determined packets to test ZomB. Sends to localhost
+    /// </summary>
+    public class DashboardTCPEmulator
+    {
+        byte[][] pkts;
+        int i = 0;
+        Thread t;
+        TcpListener listen;
+
+        /// <summary>
+        /// Create a new V-cRIO
+        /// </summary>
+        public DashboardTCPEmulator()
+        {
+            pkts = new byte[][] { };
+        }
+
+        /// <summary>
+        /// Kill the V-cRIO
+        /// </summary>
+        ~DashboardTCPEmulator()
+        {
+            Stop();
+        }
+
+        /// <summary>
+        /// Gets or sets the array of byte[1018] packets to send
+        /// </summary>
+        public byte[][] Packets
+        {
+            get
+            {
+                return pkts;
+            }
+            set
+            {
+                if (pkts == null)
+                    pkts = new byte[][] { };
+                lock (pkts)
+                {
+                    pkts = value;
+                    i = 0;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Starts the packet sending to localhost
+        /// </summary>
+        public void Start()
+        {
+            if (Packets != null)
+            {
+                t = new Thread(dowork);
+                t.IsBackground = true;
+                t.Start();
+            }
+        }
+
+        void dowork()
+        {
+            listen = new TcpListener(new IPEndPoint(IPAddress.Loopback, 9066));
+            listen.Start();
+            i = 0;
+            while (true)
+            {
+                try
+                {
+                    var client = listen.AcceptTcpClient();
+                    var stream = client.GetStream();
+                    stream.Write(new byte[] { 0x45, 0x00 }, 0, 2);
+                    while (client.Connected)
+                    {
+                        Thread.Sleep(20);
+                        lock (pkts)
+                        {
+                            if (i >= Packets.Length)
+                                i = 0;
+                            stream.Write(Packets[i], 0, Packets[i].Length);
+                            i++;
+                        }
+                    }
+                }
+                catch { }
+            }
+        }
+
+        /// <summary>
+        /// Stop packet sending
+        /// </summary>
+        public void Stop()
+        {
+            try
+            {
+                t.Abort();
+            }
+            catch { }
+        }
+    }
 }
