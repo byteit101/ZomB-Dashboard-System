6c7b46848a683d2bc8664db051c82887da8c505e
byteit101 <byteit101@0141d0df-edca-4edd-a601-3402401f7f08>
2012-01-16 04:46:54 +0000

    CSV logger!
    git-svn-id: http://firstforge.wpi.edu/svn/repos/zomb/trunk@489 0141d0df-edca-4edd-a601-3402401f7f08

---
+++ ZomBdll/NetAndDataProcessing/DashboardData/CsvDataMonitor.cs
@@ -0,0 +1,284 @@
+﻿/*
+ * ZomB Dashboard System <http://firstforge.wpi.edu/sf/projects/zombdashboard>
+ * Copyright (C) 2009-2010, Patrick Plenefisch and FIRST Robotics Team 451 "The Cat Attack"
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Text;
+using System451.Communication.Dashboard.Net;
+using System451.Communication.Dashboard.Utils;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.Linq;
+
+namespace System451.Communication.Dashboard
+{
+    /// <summary>
+    /// DataSaver saves data from the DDH to a file that can be read by DataPlayerSource
+    /// </summary>
+    [DataSource("CsvSaver", "Saves all data as a csv. Usage: zomb://.0/CsvSaver/[C:\\Path.to.file]")]
+    public class CsvSaver : IZomBMonitor
+    {
+        StringCollection header = new StringCollection();
+        Collection<StringCollection> dataq = new Collection<StringCollection>();
+        int qindx = 0;
+        bool saving = false;
+        StreamWriter outs;
+        uint lasttime, savetime;
+        DateTime starttime;
+        string fpath;
+
+        /// <summary>
+        /// Creates a new DataSaver
+        /// </summary>
+        /// <param name="file">A file to save to</param>
+        public CsvSaver(string file)
+        {
+            fpath = file;
+        }
+
+        internal CsvSaver(ZomBUrl url)
+        {
+            var urlPath = url.Path.Substring(1);//remove first char
+            if (urlPath == "")
+                fpath = BTZomBFingerFactory.DefaultSaveLocation + "\\ZCapture" + (DateTime.Now.Ticks.ToString("x")) + ".csv";
+            else if (!urlPath.Contains(":\\"))//not absoloute
+                fpath = BTZomBFingerFactory.DefaultSaveLocation + "\\" + urlPath;
+            else
+                fpath = urlPath;
+        }
+
+        ~CsvSaver()
+        {
+            Stop();
+            WriteOff();
+        }
+
+        /// <summary>
+        /// Are we saving
+        /// </summary>
+        public bool Running
+        {
+            get
+            {
+                return saving;
+            }
+        }
+
+
+        private void AddLookupData(string prefix, ZomBDataLookup datal)
+        {
+            if (saving)
+            {
+                lock (dataq)
+                {
+                    var scl = new StringCollection();
+                    foreach (var item in datal)
+                    {
+                        if (item.Value.Value is ZomBDataLookup)
+                        {
+                            AddLookupData(prefix + item.Key + ".", item.Value.Value as ZomBDataLookup);
+                            continue;
+                        }
+                        if (!header.Contains(prefix + item.Key))
+                        {
+                            header.Add(prefix + item.Key);
+                        }
+                        int hidx = header.IndexOf(prefix + item.Key);
+                        while (scl.Count <= hidx)
+                        {
+                            scl.Add(null);
+                        }
+                        scl[hidx] = item.Value.ToString();
+                    }
+                    if (dataq.Count > 0 && GetTime() - lasttime < 50)//less than 50ms
+                    {
+                        var mmine = new string[scl.Count];
+                        scl.CopyTo(mmine, 0);
+                        if (Mergable(ref scl))
+                        {
+                            dataq[dataq.Count - 1] = scl;
+                        }
+                        else
+                        {
+                            scl = new StringCollection();
+                            scl.AddRange(mmine);
+                            dataq.Add(scl);
+                        }
+                    }
+                    else
+                    {
+                        dataq.Add(scl);
+                        lasttime = GetTime();
+                    }
+                }
+            }
+        }
+
+        private bool Mergable(ref StringCollection scl)
+        {
+            var dq = dataq[dataq.Count - 1];
+            while (scl.Count < dq.Count)
+            {
+                scl.Add(dq[scl.Count]);
+            }
+            for (int i = 0; i < dq.Count; i++)
+            {
+                if (scl[i] == null && dq[i] != null)
+                {
+                    scl[i] = dq[i];
+                }
+                if (scl[i] != dq[i] && dq[i] != null)
+                    return false;
+            }
+            return true;
+        }
+
+        private uint GetTime()
+        {
+            TimeSpan dif = DateTime.Now.Subtract(starttime);
+            return (uint)(dif.TotalMinutes * 60000);//ms
+        }
+
+        private void WriteBuffer()
+        {
+            lock (dataq)
+            {
+                try
+                {
+                    foreach (var item in header)
+                    {
+                        outs.Write(item + ",");
+                    }
+                    outs.WriteLine();
+                    for (int i = 0; i < dataq.Count - 1; i++)
+                    {
+                        foreach (var item in dataq[i])
+                        {
+                            outs.Write((item ?? "") + ",");
+                        }
+                        outs.WriteLine();
+                    }
+
+                    //save last for next merge
+                    var last = dataq[dataq.Count - 1];
+                    dataq.Clear();
+                    dataq.Add(last);
+                    savetime = GetTime();
+                }
+                catch { }
+            }
+        }
+
+        /// <summary>
+        /// Start saving
+        /// </summary>
+        public void Start()
+        {
+            if (!Running)
+            {
+                outs = new StreamWriter(FilePath, true);
+                saving = true;
+                starttime = DateTime.Now;
+                lasttime = savetime = 0;
+            }
+        }
+
+        /// <summary>
+        /// Stop Saving
+        /// </summary>
+        public void Stop()
+        {
+            if (Running)
+            {
+                saving = false;
+                WriteBuffer();
+                WriteOff();
+            }
+        }
+
+        private void WriteOff()
+        {
+            if (outs != null)
+            {
+                lock (outs)
+                {
+                    try
+                    {
+                        outs.Flush();
+                        outs.Close();
+                        outs = null;
+                    }
+                    catch { }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Where to save the file
+        /// </summary>
+        [Category("ZomB"), Description("Where to save the files")]
+        public string FilePath
+        {
+            get { return fpath; }
+            set { fpath = value; }
+        }
+
+        #region IZomBMonitor Members
+
+        /// <summary>
+        /// IZomBMonitor: Updates the status
+        /// </summary>
+        /// <param name="status">The new status</param>
+        public void UpdateStatus(FRCDSStatus status)
+        {
+            //ignore status
+        }
+
+        /// <summary>
+        /// IZomBMonitor: Updates the data
+        /// </summary>
+        /// <param name="data">The new data</param>
+        public void UpdateData(ZomBDataLookup data)
+        {
+            if (!saving)
+            {
+                Start();
+            }
+            try
+            {
+                AddLookupData("", data);
+                if (dataq.Count > 100 || lasttime - savetime > 10000)
+                    WriteBuffer();
+            }
+            catch { }
+
+        }
+
+        #endregion
+
+        /// <summary>
+        /// Magic method for zomb:// urls
+        /// </summary>
+        /// <returns></returns>
+        private static ZomBUrlInfo GetZomBUrlInfo()
+        {
+            return new ZomBUrlInfo { DefaultPort = 0 };
+        }
+    }
+}
